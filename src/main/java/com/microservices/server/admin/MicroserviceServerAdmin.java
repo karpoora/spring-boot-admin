9375838afc84aab6c484fe71f3f38467c2d71ca3
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-12-11 00:50:23 +0530

    correction in application name

--- src/main/java/com/microservices/server/admin/MicroserviceServerAdmin.java
+++ src/main/java/com/microservices/server/admin/MicroserviceServerAdmin.java
@@ -4,12 +4,14 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
+@EnableEurekaClient
 //@EnableAZSSO
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MicroserviceServerAdmin {
