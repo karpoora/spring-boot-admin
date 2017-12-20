49c74b4528a6cf65f96774d6972110dab818695d
Sundarapandian, Karpoora <karpoora.sundarapandian@astrazeneca.com>
2017-12-20 13:47:07 +0530

    removed @EurekaClient

--- src/main/java/com/microservices/server/admin/MicroserviceServerAdmin.java
+++ src/main/java/com/microservices/server/admin/MicroserviceServerAdmin.java
@@ -4,16 +4,12 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @EnableAdminServer
-@EnableEurekaClient
-//@EnableAZSSO
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class MicroserviceServerAdmin {
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceServerAdmin.class, args);
